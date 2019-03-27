c4808f4069692dfae3445ec37770a530afe27bcd
exblematique <yoann.chappaz@outlook.com>
2019-03-27 09:47:53 +0100

    RFID works and program Python in DB too

--- install_PC.sh
+++ install_PC.sh
@@ -5,9 +5,14 @@ then
     echo 'Sorry, you must be a root'
 
 else
-    apt-get update && apt-get install python-pip python3-pip twine
-    apt-get install mosquitto -y
-    apt-get install mosquitto-clients -y
+    apt-get update
+    apt-get install python-pip python3-pip twine -y
     pip2 install -r requirements.txt
     pip3 install -r requirements.txt
+
+    #Installing mosquitto broker :
+    apt-get install mosquitto mosquitto-clients -y
+    echo "listener 1883" >> /etc/mosquitto/mosquitto.conf
+    pip install paho-mqtt
+    pip3 install paho-mqtt
 fi
